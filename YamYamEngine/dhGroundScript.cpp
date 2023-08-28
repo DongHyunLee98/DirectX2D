a99141e9d049a757971c1948e7958a28b66d2e2d
DongHyunLee98 <godsh88@gmail.com>
2023-08-29 00:59:43 +0900

    bullet Destroy point

--- YamYamEngine/dhGroundScript.cpp
+++ YamYamEngine/dhGroundScript.cpp
@@ -17,7 +17,11 @@ namespace dh
 
 		if (other->GetOwner()->GetName() == L"playerBullet")
 		{
-			object::Destroy(other->GetOwner());
+			// object::Destroy(other->GetOwner());
 		}
+	}
+	void GroundScript::OnCollisionExit(Collider2D* other)
+	{
+		object::Destroy(other->GetOwner());
 	}
 }
--- YamYamEngine/dhGroundScript.h
+++ YamYamEngine/dhGroundScript.h
@@ -10,6 +10,7 @@ namespace dh
 		virtual void Initialize() override;
 		virtual void Update() override;
 		virtual void OnCollisionEnter(Collider2D* other) override;
+		virtual void OnCollisionExit(Collider2D* other) override;
 	};
 };
 
